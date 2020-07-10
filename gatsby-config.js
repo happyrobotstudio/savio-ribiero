2b42b3c28706c37a05185f7c76421b65ba8cfcd8
happyrobot <happyrobotstudio@gmail.com>
2020-07-10 15:14:48 +1000

    Scheme update

--- gatsby-config.js
+++ gatsby-config.js
@@ -46,5 +46,20 @@ module.exports = {
     // `gatsby-plugin-offline`,
     `gatsby-plugin-netlify`,
     `gatsby-plugin-netlify-cms`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/static/assets`,
+        name: 'images',
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `content`,
+        path: `${__dirname}/content`,
+      },
+    },
+    'gatsby-transformer-remark',
   ],
 }
